62939e9bb6d0740130bd5af8b3706075010a6320
Nikolay Smk <nikolaysmyk@icloud.com>
2023-12-19 22:23:25 +0300

    add containsDublicate

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -7,5 +7,23 @@
 //   return set.size !== nums.length
 // };
 //
-const containsDuplicate = (nums) => {
-
+// var containsDuplicate = function(nums) {
+//   const map = new Map()
+//   for (let i = 0; i < nums.length; i++) {
+//     if (map.has(nums[i])) {
+//       return true
+//     }
+//     map.set(nums[i], i)
+//   }
+//   return false
+// };
+var containsDuplicate = function(nums) {
+  const set = new Set()
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true
+    }
+    set.add(nums[i])
+  }
+  return false
+};
