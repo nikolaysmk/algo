d811db601fb149eff1251a96e04111a0ab2265d3
Nikolay Smk <nikolaysmyk@icloud.com>
2023-12-29 13:55:06 +0300

    Create maxProfit.js

---
+++ maxProfit.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+  let l = 0 //buy
+  let r = 1 // sell
+  let maxProfit = 0
+
+  while (r < prices.length) {
+    if (prices[l] < prices[r]) {
+      profit = prices[r] - prices[l]
+      maxProfit = Math.max(maxProfit, profit)
+    } else {
+      l = r
+    }
+    r += 1
+  }
+  return maxProfit
+
+};
