ff87a0b4d7dbddd443a530ec6bff76fed942dbab
Nikolay Smk <nikolaysmyk@icloud.com>
2024-01-10 18:11:13 +0300

    Update isPalindromeString.js

--- isPalindromeString.js
+++ isPalindromeString.js
@@ -2,24 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-  const clean = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
-  let left = 0
-  let right = clean.length - 1
+var isPalindrome = function (s) {
+  const clean = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = clean.length - 1;
 
   while (left < right) {
     if (clean[left] !== clean[right]) {
-      return false
+      return false;
     }
-    left++
-    right--
+    left++;
+    right--;
   }
 
-
-  return true
+  return true;
 };
-
-
 
 // let clean = '';
 //
