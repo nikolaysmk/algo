ba1933014a7a203291ef74bdf6c00733379b6464
Nikolay Smk <nikolaysmyk@icloud.com>
2024-01-17 17:32:15 +0300

    Update uniqueOccurrences1207.js

--- uniqueOccurrences1207.js
+++ uniqueOccurrences1207.js
@@ -2,14 +2,42 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var uniqueOccurrences = function (arr) {
-  const countMap = {};
+// var uniqueOccurrences = function (arr) {
+//   const countMap = {};
+//
+//   arr.forEach((num) => (countMap[num] = (countMap[num] || 0) + 1));
+//
+//   const occurrencesSet = new Set();
+//
+//   for (const count of Object.values(countMap)) {
+//     if (occurrencesSet.has(count)) {
+//       return false;
+//     }
+//     occurrencesSet.add(count);
+//   }
+//
+//   return true;
+// };
+//
 
-  arr.forEach((num) => (countMap[num] = (countMap[num] || 0) + 1));
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+  const countMap = new Map();
+
+  arr.forEach((num) => {
+    if (countMap.has(num)) {
+      countMap.set(num, countMap.get(num) + 1);
+    } else {
+      countMap.set(num, 1);
+    }
+  });
 
   const occurrencesSet = new Set();
 
-  for (const count of Object.values(countMap)) {
+  for (const [_val, count] of countMap) {
     if (occurrencesSet.has(count)) {
       return false;
     }
